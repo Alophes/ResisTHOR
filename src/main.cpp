9dad0b362a9a6aa0fca4e57b105b8e31b2caadcd
Alophes <bryan.gasse@gmail.com>
2023-09-27 18:09:10 -0400

    Initiate 2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <librobus.h>
+#include <LibRobus.h>
 
 #define PIN_CH1 34
 #define PIN_CH2 35
@@ -18,15 +18,17 @@ void setupVex(uint8_t _PIN_CH1,uint8_t _PIN_CH2);
 void setupMotorControl(uint8_t _PIN_PWM_D,uint8_t _PIN_DIR_D, uint8_t _PIN_PWM_G,uint8_t _PIN_DIR_G);
 void foward(float _speed);
 
-
-
 void setup() {
-  setupVex(PIN_CH1,PIN_CH2);
-  setupMotorControl(PIN_PWM_D,PIN_DIR_D,PIN_PWM_G,PIN_DIR_G);
+  //setupVex(PIN_CH1,PIN_CH2);
+  //setupMotorControl(PIN_PWM_D,PIN_DIR_D,PIN_PWM_G,PIN_DIR_G);
+  BoardInit();
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(100);
+  Serial.println(ENCODER_Read(1));
 }
 
 void setupVex(uint8_t _PIN_CH1,uint8_t _PIN_CH2){
