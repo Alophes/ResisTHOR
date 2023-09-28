a8eed20b6fdab5aa3f2274554f8c4f0e668903f5
MatisGoulet <Matis.goulet@gmail.com>
2023-09-28 12:10:53 -0400

    setCallback

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define ENCODER_LEFT 0
 #define ENCODER_RIGHT 1
@@ -10,6 +12,8 @@
 void foward(float _speed);
 void stopMotors();
 void motorsAccelerate(float _speedWanted);
+void motorsDeccelerate(float ActualSpeed);
+void checkPulse();
 
 struct Pulse {
   uint16_t right;
@@ -22,19 +26,22 @@ struct Pulse {
 
 struct Pulse pulse;
 float speed;
+bool doOnce = true;
+void (*foo)();
+foo = &checkPulse;
 void setup() {
   BoardInit();
   Serial.begin(9600);
   speed=0.75;
   motorsAccelerate(speed);
+  SOFT_TIMER_SetCallback(0, (*foo)());
+  SOFT_TIMER_SetDelay(0,10);
+  SOFT_TIMER_SetRepetition(0,-1);
+  SOFT_TIMER_Enable(0);
 }
 
 void loop() {
-  //Serial.println(ENCODER_Read(1));
-  pulse.leftPast=pulse.left;
-  pulse.rightPast=pulse.right;
-  pulse.right = ENCODER_Read(ENCODER_RIGHT);
-  pulse.left = ENCODER_Read(ENCODER_LEFT);
+  SOFT_TIMER_Update();
   if (pulse.left >= 3200) {
     ENCODER_Reset(ENCODER_LEFT);
     pulse.leftCnt++;
@@ -43,13 +50,46 @@ void loop() {
     ENCODER_Reset(ENCODER_RIGHT);
     pulse.rightCnt++;
   }
-  if(pulse.rightCnt >= 53 && pulse.leftCnt >= 53)
-    stopMotors();
+  if(pulse.rightCnt >= 18 || pulse.leftCnt >= 18) {
+    if(doOnce == true){
+      doOnce = false;
+      motorsDeccelerate(speed);
+    }
+    else
+      stopMotors();
+  }
   else
     foward(speed);
 
 }
-
+void checkPulse(){
+  pulse.leftPast=pulse.left;
+  pulse.rightPast=pulse.right;
+  pulse.left = ENCODER_Read(ENCODER_LEFT);
+  pulse.right = ENCODER_Read(ENCODER_RIGHT);
+}
+void motorsDeccelerate(float ActualSpeed){
+  uint8_t delayMs = 75;
+  foward(ActualSpeed*0.90);
+  delay(delayMs);
+  foward(ActualSpeed*0.80);
+  delay(delayMs);
+  foward(ActualSpeed*0.70);
+  delay(delayMs);
+  foward(ActualSpeed*0.60);
+  delay(delayMs);
+  foward(ActualSpeed*0.50);
+  delay(delayMs);
+  foward(ActualSpeed*0.40);
+  delay(delayMs);
+  foward(ActualSpeed*0.30);
+  delay(delayMs);
+  foward(ActualSpeed*0.20);
+  delay(delayMs);
+  foward(ActualSpeed*0.10);
+  delay(delayMs);
+  stopMotors();
+}
 void motorsAccelerate(float _speedWanted){
   uint8_t delayMs = 150;
   foward(_speedWanted*0.10);
@@ -74,12 +114,12 @@ void motorsAccelerate(float _speedWanted){
 }
 void foward(float _speed){
   if(pulse.right < pulse.left) {
-    MOTOR_SetSpeed(MOTOR_RIGHT,_speed+0.15);
-    MOTOR_SetSpeed(MOTOR_LEFT,_speed-0.15);
+    MOTOR_SetSpeed(MOTOR_RIGHT,_speed+0.005);
+    //MOTOR_SetSpeed(MOTOR_LEFT,_speed-0.05);
   }
   if(pulse.right > pulse.left) {
-    MOTOR_SetSpeed(MOTOR_LEFT,_speed+0.15);
-    MOTOR_SetSpeed(MOTOR_RIGHT,_speed-0.15);
+    MOTOR_SetSpeed(MOTOR_LEFT,_speed+0.005);
+    //MOTOR_SetSpeed(MOTOR_RIGHT,_speed-0.05);
   }
   if(pulse.right == pulse.left) {
     MOTOR_SetSpeed(MOTOR_LEFT,_speed);
@@ -94,7 +134,6 @@ void foward(float _speed){
   Serial.println(pulse.right);
   }
 }
-
 
 void stopMotors(){
  MOTOR_SetSpeed(MOTOR_LEFT,0);
