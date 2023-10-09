78bae3ea294e6d28519a5e9fc4ecfd8aec2918fa
MatisGoulet <Matis.goulet@gmail.com>
2023-10-08 21:04:02 -0400

    fixed bugs logic + optimisation

--- src/main.cpp
+++ src/main.cpp
@@ -11,123 +11,122 @@
 
 #define MAX_SPEED 0.5
 #define TURNING_SPEED 0.2
+#define MAX_TURNING_DISTANCE 1925
+#define START_POSITION 1943
+#define KP 0.005
 
+//x axis positions
 #define LEFTX 0
 #define MIDDLEX 1
 #define RIGHTX 2
 
-#define FOWARD 2
 
-const int UNIT_SIZE = 4000;
-
-// #define ENCODER_LEFT 0
-// #define ENCODER_RIGHT 1
-
-// #define MOTOR_LEFT 0
-// #define MOTOR_RIGHT 1
-
+const uint16_t UNIT_SIZE = 4250;
 
 // Functions related to the sound sensor
-
 int getRightProx() { return digitalRead(PIN_PROX_RIGHT); }
 int getLeftProx() { return digitalRead(PIN_PROX_LEFT); }
 
-
-// Function related to the proximity sensor
-
+// Function related to the proximity sensor                       pourquoi des float et pas des int?
 float getAmbient() { return analogRead(PIN_SND_AMB); }
 float getFrequency() { return analogRead(PIN_SND_FRQ); }
 
 int detectFrequency() { return (-(getAmbient() - 45) + getFrequency() > 50); }
 
 void stop(){MOTOR_SetSpeed(RIGHT, 0);MOTOR_SetSpeed(LEFT, 0);delay(100);}
-void turnRight(){
-	int leftDistance = 0, rightDistance = 0;
+
+void getInPosition(){
+	uint16_t leftDistance = 0, rightDistance = 0;
 	float leftSpeed, rightSpeed;
 	ENCODER_Reset(RIGHT);
 	ENCODER_Reset(LEFT);
-	rightSpeed = -TURNING_SPEED;
+	rightSpeed = TURNING_SPEED; 
 	leftSpeed = TURNING_SPEED;
-	// Moving right wheel
-	while (rightDistance < 1900) {
+	while (rightDistance < START_POSITION && leftDistance < START_POSITION) {
 		MOTOR_SetSpeed(RIGHT, rightSpeed);
-		rightDistance = -ENCODER_Read(RIGHT);
-		leftDistance = ENCODER_Read(LEFT);
-		delay(20);
-	}
-	// Making sure the robot stops
-	MOTOR_SetSpeed(RIGHT, 0);
-	delay(100);
-	// Moving left wheel
-	while (leftDistance < 1900) {
 		MOTOR_SetSpeed(LEFT, leftSpeed);
-		rightDistance = -ENCODER_Read(RIGHT);
+		rightDistance = ENCODER_Read(RIGHT);
 		leftDistance = ENCODER_Read(LEFT);
 		delay(20);
 	}
-	// Making sure the robot stops
-	MOTOR_SetSpeed(LEFT, 0);
+	stop();
 	delay(100);
 }
-void turnLeft(){
-	int leftDistance = 0, rightDistance = 0;
+void turn(bool dir){
+	uint16_t leftDistance = 0, rightDistance = 0;
 	float leftSpeed, rightSpeed;
 	ENCODER_Reset(RIGHT);
 	ENCODER_Reset(LEFT);
-	rightSpeed = TURNING_SPEED;
-	leftSpeed = -TURNING_SPEED;
-	// Moving left wheel
-	while (leftDistance < 1900) {
-		MOTOR_SetSpeed(LEFT, leftSpeed);
-		rightDistance = ENCODER_Read(RIGHT);
-		leftDistance = -ENCODER_Read(LEFT);
-		delay(20);
+	if(dir == RIGHT){
+		rightSpeed = -TURNING_SPEED; 
+		leftSpeed = TURNING_SPEED;
+		// Moving right wheel
+		while (rightDistance < MAX_TURNING_DISTANCE) {
+			MOTOR_SetSpeed(RIGHT, rightSpeed);
+			rightDistance = -ENCODER_Read(RIGHT);
+			leftDistance = ENCODER_Read(LEFT);
+			delay(20);
+		}
+		// Making sure the robot stops
+		MOTOR_SetSpeed(RIGHT, 0);
+		delay(100);
+		// Moving left wheel
+		while (leftDistance < MAX_TURNING_DISTANCE) {
+			MOTOR_SetSpeed(LEFT, leftSpeed);
+			rightDistance = -ENCODER_Read(RIGHT);
+			leftDistance = ENCODER_Read(LEFT);
+			delay(20);
+		}
+		// Making sure the robot stops
+		MOTOR_SetSpeed(LEFT, 0);
 	}
-	// Making sure the robot stops
-	MOTOR_SetSpeed(LEFT, 0);
-	delay(100);
-	// Moving right wheel
-	while (rightDistance < 1900) {
-		MOTOR_SetSpeed(RIGHT, rightSpeed);
-		rightDistance = ENCODER_Read(RIGHT);
-		leftDistance = -ENCODER_Read(LEFT);
-		delay(20);
+	else{
+		rightSpeed = TURNING_SPEED;
+		leftSpeed = -TURNING_SPEED;
+		// Moving left wheel
+		while (leftDistance < MAX_TURNING_DISTANCE) {
+			MOTOR_SetSpeed(LEFT, leftSpeed);
+			rightDistance = ENCODER_Read(RIGHT);
+			leftDistance = -ENCODER_Read(LEFT);
+			delay(20);
+		}
+		// Making sure the robot stops
+		MOTOR_SetSpeed(LEFT, 0);
+		delay(100);
+		// Moving right wheel
+		while (rightDistance < MAX_TURNING_DISTANCE) {
+			MOTOR_SetSpeed(RIGHT, rightSpeed);
+			rightDistance = ENCODER_Read(RIGHT);
+			leftDistance = -ENCODER_Read(LEFT);
+			delay(20);
 
+		}
+		// Making sure the robot stops
+		MOTOR_SetSpeed(RIGHT, 0);
 	}
-	// Making sure the robot stops
-	MOTOR_SetSpeed(RIGHT, 0);
 	delay(100);
 }
-
 void oneEighty(bool direction){
-	if(direction == LEFT){
-		for(uint8_t i = 0; i < 2; i++){
-			turnLeft();
-		}
+	for(uint8_t i = 0; i < 2; i++){
+		turn(direction);
 	}
-	else if(direction == RIGHT){
-		for(uint8_t i = 0; i < 2; i++){
-			turnRight();
-		}
-	}
-
 }
 void advanceUnit() {
-	int mainDistance = 0, leftDistance = 0, rightDistance = 0;
+	uint16_t mainDistance = 0, leftDistance = 0, rightDistance = 0;
 	float leftSpeed, rightSpeed;
 	ENCODER_Reset(RIGHT);
 	ENCODER_Reset(LEFT);
 	rightSpeed = MAX_SPEED;
 	leftSpeed = MAX_SPEED; 
 	// Accelerate
-	for (int i = 0; i < 10; i += 2) {
+	for (uint8_t i = 0; i < 10; i += 2) {
 		rightDistance = ENCODER_ReadReset(RIGHT);
 		leftDistance = ENCODER_ReadReset(LEFT);
 		MOTOR_SetSpeed(RIGHT, (i * 0.1) * rightSpeed);
 		MOTOR_SetSpeed(LEFT, (i * 0.1) * leftSpeed);
 		// Self correcting
-		if (rightDistance < leftDistance) { rightSpeed += 0.01; leftSpeed -= 0.01; }
-		else { rightSpeed -= 0.01; leftSpeed += 0.01; }
+		if (rightDistance < leftDistance) { rightSpeed += KP; leftSpeed -= KP; }
+		else { rightSpeed -= KP; leftSpeed += KP; }
 		delay(100);
 	}
 	// Updating distance traveled
@@ -137,8 +136,8 @@ void advanceUnit() {
 		rightDistance = ENCODER_ReadReset(RIGHT);
 		leftDistance = ENCODER_ReadReset(LEFT);
 		// Self correcting
-		if (rightDistance < leftDistance) { rightSpeed += 0.005; leftSpeed -= 0.005; }
-		else { rightSpeed -= 0.005; leftSpeed += 0.005; }
+		if (rightDistance < leftDistance) { rightSpeed += KP; leftSpeed -= KP; }
+		else { rightSpeed -= KP; leftSpeed += KP; }
 
 		MOTOR_SetSpeed(RIGHT, rightSpeed);
 		MOTOR_SetSpeed(LEFT, leftSpeed);
@@ -149,15 +148,15 @@ void advanceUnit() {
 
 	}
 	// Deccelerate
-	for (int i = 10; i > 0; i -= 2) {
+	for (uint8_t i = 10; i > 0; i -= 2) {
 		rightDistance = ENCODER_ReadReset(RIGHT);
 		leftDistance = ENCODER_ReadReset(LEFT);
 		MOTOR_SetSpeed(RIGHT, (i * 0.1) * rightSpeed);
 		MOTOR_SetSpeed(LEFT, (i * 0.1) * leftSpeed);
 
 		// Self correcting
-		if (rightDistance < leftDistance) { rightSpeed += 0.01; leftSpeed -= 0.01; }
-		else { rightSpeed -= 0.01; leftSpeed += 0.01; }
+		if (rightDistance < leftDistance) { rightSpeed += KP; leftSpeed -= KP; }
+		else { rightSpeed -= KP; leftSpeed += KP; }
 		delay(100);
 	}
 	// Making sure the robot stops
@@ -166,68 +165,68 @@ void advanceUnit() {
 bool go = false;
 void setup() {
 	BoardInit();
-	delay(2000);
 }
 uint8_t posY = 0;//default
 uint8_t posX = MIDDLEX;//default
+
 void loop() {
-	delay(100);
-	if(detectFrequency()){Serial.println(detectFrequency());go = true;}
+	if(detectFrequency()){Serial.println(detectFrequency());go = true; getInPosition();}
 	if(go == true){
 		if(getRightProx()==0||getLeftProx()==0){
-			if(posY<10){
+			if(posY<9){
 				switch (posX)
 				{
 					case MIDDLEX:
-						turnLeft();
+						turn(LEFT);
 						if(getRightProx()==0||getLeftProx()==0){
 							oneEighty(RIGHT);
 							advanceUnit();
-							turnLeft();
+							turn(LEFT);
 							posX = RIGHTX;
 						}
 						else{
 							advanceUnit();
-							turnRight();
+							turn(RIGHT);
 							posX = LEFTX;
 						}
 						if(getRightProx()==0||getLeftProx()==0){
-							turnRight();
+							turn(RIGHT);
 							for(uint8_t i = 0; i < 2; i++){advanceUnit();}
-							turnLeft();
+							turn(LEFT);
 							posX = RIGHTX;
 						}
 						break;
 					case LEFTX:
-						turnRight();
+						turn(RIGHT);
 						advanceUnit();
-						turnLeft();
+						turn(LEFT);
 						posX = MIDDLEX;
 						if(getRightProx()==0||getLeftProx()==0){
-							turnRight();
+							turn(RIGHT);
 							advanceUnit();
-							turnLeft();
+							turn(LEFT);
 							posX = RIGHTX;
 						}
 						break;
 					case RIGHTX:
-						turnLeft();
+						turn(LEFT);
 						advanceUnit();
-						turnRight();
+						turn(RIGHT);
 						posX = MIDDLEX;
 						if(getRightProx()==0||getLeftProx()==0){
-							turnLeft();
+							turn(LEFT);
 							advanceUnit();
-							turnRight();
+							turn(RIGHT);
 							posX = LEFTX;
 						}
 						break;
 				}
-				Serial.println(posX);
 			}
 		}
 		else{
-			if(posY < 9){advanceUnit();posY++;}else{go = false;stop();}
+			if(posY < 9){advanceUnit(); posY++;}
+			else{go = false; stop(); posY = 0; posX = MIDDLEX;}
 		}
 	}	
+	delay(100);
 }
